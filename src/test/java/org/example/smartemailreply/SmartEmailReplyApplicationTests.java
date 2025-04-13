0457fe467d00e2250657f7b00d25e8e41ba20a1f
Harjas Singh <83627055+harjasae2001@users.noreply.github.com>
2025-04-13 23:29:26 +0530

    first commit

---
+++ src/test/java/org/example/smartemailreply/SmartEmailReplyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartemailreply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEmailReplyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
